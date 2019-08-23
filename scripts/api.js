ecc7b792d41a3eb0e46edb5d08ef35c36c826456
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-08-23 09:54:18 -0700

    completed fetch the items and add them to the store

--- scripts/api.js
+++ scripts/api.js
@@ -1,13 +1,31 @@
-const api = (function(){
-    const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jamesl'
-    const getItems = function(){
-      return Promise.resolve('A successful response!');
+'use strict';
 
-    }
-    return{
-      getItems,
-    }
-})()
+const api = (function(){
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/jamesl';
+  const getItems = function(){
+    fetch(`${BASE_URL}/items`)
+    //return Promise.resolve('A successful response!');
+      .then(res => res.json())
+      .then(resJson => console.log(resJson)); 
+  };
+  const createItem = function(name) {
+    let newItem = {
+      name: name
+    };
+    
+    return fetch(`${BASE_URL}/items`, {
+      method: 'POST',
+      headers: new Headers ({
+        'Content-Type': 'application/json'
+      }),
+      body: JSON.stringify(newItem)
+    });
+  };
+  return{
+    getItems,
+    createItem,
+  };
+})();
 
 
 
--- scripts/index.js
+++ scripts/index.js
@@ -4,11 +4,21 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 });
 
-store.items.push(Item.create('apples'));
+api.createItem('pears')
+  .then(res => res.json())
+  .then((newItem) => {
+    return api.getItems();
+  })
+  .then(res => res.json())
+  .then((items) => {
+    console.log(items);
+  });
 
-api.getItems()
-  .then(res => console.log(res));
-
-console.log(api.BASE_URL);
